5e9e10f283e822ae204e59d800be99456d46e177
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-09-13 14:13:38 +0000

    Removed secret key

--- msp4/settings.py
+++ msp4/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure--#a7(i_n$#5crq*datt@xk!s935#cqaz#d&uo+9bcl=vw+j=3&')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
