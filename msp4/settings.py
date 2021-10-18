a5b1327cae08245c14d29767c05b939e858803ee
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-10-18 12:36:22 +0000

    Trying deploy again

--- msp4/settings.py
+++ msp4/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET = 'milestoneproject4'
+    AWS_STORAGE_BUCKET_NAME = 'milestoneproject4'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
