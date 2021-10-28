5dde358b5f1003a2b697d3b1fa44fed3a97bc71d
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-10-28 22:37:35 +0000

    Added style to checkout form and changed hero img

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
 		mybutton.style.display = "block";
 	} else {
 		mybutton.style.display = "none";
