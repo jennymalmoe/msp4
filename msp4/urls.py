821b794d914f2789e696f7e42d197a9962b2b494
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-11-21 22:43:55 +0000

    Created an about us app

--- msp4/urls.py
+++ msp4/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('products/', include('products.urls')),
     path('profile/', include('profiles.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
