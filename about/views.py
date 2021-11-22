2330f551b37bad9baedb1da08a5b466be4c5ec70
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-11-22 12:43:38 +0000

    Added contact form in about us page

--- about/views.py
+++ about/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_about(request):
+    """ A view that renders the about contents page """
+
+    return render(request, 'about/about.html')
