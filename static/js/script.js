1d73d3a4eea0e1ada8922e64a693d6c5b4fa36c7
Jenny Malmoe <jenny.c.borglin@gmail.com>
2021-11-30 20:00:04 +0000

    Added new contact form

--- static/js/script.js
+++ static/js/script.js
@@ -34,10 +34,60 @@ let toastList = toastElList.map(function (toastEl) {
   bsToast.show();
 })
 
-// Sweet Alert2
-Swal.fire({
-	title: 'Error!',
-	text: 'Do you want to continue',
-	icon: 'error',
-	confirmButtonText: 'Cool'
-  })
+
+// ALERT
+
+//EmailJs used to send email from contact form
+
+/* Checking if form is valid 
+Sourced from https://codepen.io/tetnuc/pen/gRqOEO */
+$('#contactForm').validate({
+    rules: {
+        uname: {
+            required: true
+        },
+        uemail: {
+            required: true
+        },
+        umessage: {
+            required: true
+        }
+    },
+    messages: {
+        uname: {
+            required: "Please complete all fields"
+        },
+        uemail: {
+            required: "Please complete all fields"
+        },
+        umessage: {
+            required: "Please complete all fields"
+        }
+    },
+
+});
+
+// EmailJs Function
+$('#contactFormSubmit').click(function (event) {
+    event.preventDefault();
+    if($('#contactForm').valid()) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Thank You!',
+            text: 'Your message has been received by BOHO Team',
+            showConfirmButton: false,
+            timer: 3000
+        });
+        sendMail();
+    }
+});
+
+function sendMail(){
+    // reference: CI tutorial and https://www.youtube.com/watch?v=x7Ewtay0Q78
+    let tempParams = {
+        user_name: document.getElementById("uname").value,
+        user_email: document.getElementById("uemail").value,
+        user_msg: document.getElementById("umessage").value,
+    };
+    emailjs.send('service_k339kpr', 'template_lrrt8os', tempParams);
+} 
